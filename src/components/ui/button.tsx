a4f491108ad5b344beab555b42275fe6b92df900
john <john@247liveit.com>
2026-01-18 02:30:54 +0400

    default variant Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -45,7 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant: variant ?? 'default', size, className }))}
         ref={ref}
         {...props}
       />
