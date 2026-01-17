292da4ca068a446bbaf7cd4844a0c3062a719981
john <john@247liveit.com>
2026-01-18 01:54:34 +0400

    Fix modal ScrollArea styles and classname merging, remove snap scroll

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,7 +6,7 @@ export * from './aspect-ratio';
 export * from './avatar';
 export * from './badge';
 export * from './breadcrumb';
-export * from './button';
+export * as CButton from  './button';
 export * from './calendar';
 export * from './card';
 export * from './carousel';
